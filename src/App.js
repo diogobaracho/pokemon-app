9e8a2a96726f09047b9d4325f6c17d5095c8a3c6
Diogo Baracho <diogo.baracho@somoseducacao.com.br>
2019-11-27 16:22:54 -0300

    add apollo client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+
+
+
+
+
 import React from 'react';
+// import { render } from 'react-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import ApolloClient from 'apollo-boost';
+const client = new ApolloClient({
+  uri: 'https://graphql-pokemon.now.sh',
+});
 
 function App() {
   return (
@@ -19,6 +33,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        <ApolloProvider client={client}>
+          <div>
+            <h2>Apollo Pokemon app</h2>
+          </div>
+        </ApolloProvider>
+      </section>
     </div>
   );
 }
