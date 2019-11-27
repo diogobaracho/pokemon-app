583d312365fc2224cf4783fa36809367f69e488c
Diogo Baracho <diogo.baracho@somoseducacao.com.br>
2019-11-27 16:46:02 -0300

    Updates

---
+++ precache-manifest.47e1ffe350847813b7e683afcde8b09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ce3a425aefc55fc60edf069588c8ba",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "9492299963b32e99820f",
+    "url": "/pokemon-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "50db4fcb182e2dda461c",
+    "url": "/pokemon-app/static/js/2.2d3b9cc8.chunk.js"
+  },
+  {
+    "revision": "9492299963b32e99820f",
+    "url": "/pokemon-app/static/js/main.bbb10b16.chunk.js"
+  },
+  {
+    "revision": "0e409e05d76aee32ccbe",
+    "url": "/pokemon-app/static/js/runtime-main.cec03f6c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pokemon-app/static/media/logo.25bf045c.svg"
+  }
+]);
